44d672a9d1dc84b7100ed61b4933e171ee0366ad
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2024-12-18 10:13:51 +0100

    feat: add linuxdeploy-plugin-checkrt

---
+++ dev-utils/_linux/linuxdeploy-plugin-checkrt/linuxdeploy-plugin-checkrt.py
@@ -0,0 +1,42 @@
+import stat
+from pathlib import Path
+
+import info
+import utils
+from Package.BinaryPackageBase import BinaryPackageBase
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.targets["continous"] = "https://github.com/darealshinji/linuxdeploy-plugin-checkrt/releases/download/continuous/linuxdeploy-plugin-checkrt.sh"
+        self.targetInstallPath["continous"] = "dev-utils/bin"
+        self.description = "Have your AppImage check libgcc and libstdc++ dependencies at runtime. "
+        self.webpage = "https://github.com/darealshinji/linuxdeploy-plugin-checkrt"
+        self.defaultTarget = "continous"
+        self.patchLevel["continous"] = 1
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+
+
+class Package(BinaryPackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def unpack(self):
+        patches = self.subinfo.patchesToApply()
+        for patch in patches:
+            patchfile = self.blueprintDir() / patch[0]
+            utils.applyPatch(self.localFilePath()[0].parent, patchfile, patch[1])
+        return True
+
+    def install(self):
+        utils.createDir(self.installDir())
+        for f in self.localFilePath():
+            src = Path(f)
+            dest = Path(self.installDir()) / src.name
+            # we move the files so that on a reinstall the continuous target gets redownloaded
+            if not utils.moveFile(src, dest):
+                return False
+            dest.chmod(dest.stat().st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+        return True
